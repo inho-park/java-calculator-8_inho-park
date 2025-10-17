91f0e3048f3b7d9a493adad20703478fbcc96d16
JunSeong_Park <ttotto0608@naver.com>
2025-10-17 15:38:49 +0900

    feat: 구분자 관리 및 커스텀 구분자 등록
    서비스 구분자 관리 기능 추가
    - 커스텀 구분자 문자열 유효성 검사
    - 기본 구분자 및 커스텀 구분자 문자열 배열에 추가

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -3,9 +3,14 @@ package calculator.service;
 import calculator.model.Calculator;
 
 import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+// import java.util.List;
+// import java.util.ArrayList;
+
 
 public class CalculatorService {
-    private final Calculator calculator = new Calculator();
+    private Calculator calculator = new Calculator();
 
     /**
      * 덧셈 로직 서비스 호출
@@ -17,10 +22,10 @@ public class CalculatorService {
         String [] strArr = separateInput(input);
         String customStr = strArr[0];
         String intStr = strArr[1];
+        // 구분자는 Map으로 관리하는 것이 탐색에 더 빠를 것이라 생각해 Map으로 관리
+        Map<String, Boolean> separatorList = getSeparator(customStr);
+//        List<Integer> intArr = getIntArr(intStr);
 
-        // 커스텀 구분자 추출 로직 필요
-        // 숫자 합산 로직 필요
-//        BigInteger result = getSum(intStr);
         return new BigInteger("0");
     }
 
@@ -63,5 +68,45 @@ public class CalculatorService {
         return index;
     }
 
+    /**
+     * input => 커스텀 문자 등록
+     * @param customArr String
+     * @return Map<String, Boolean> separatorList
+     */
+    private Map<String, Boolean> getSeparator(String customArr) {
+        customArr = validCheckSeparator(customArr);
+        Map<String, Boolean> separatorList = new HashMap<>();
+        // 기본 구분자 등록
+        separatorList.put(":", true);
+        separatorList.put(",", true);
+        if (!customArr.isEmpty()) {
+            char [] chArr = customArr.toCharArray();
+            // 커스텀 구분자 등록
+            for (char c : chArr) {
+                separatorList.put(String.valueOf(c), true);
+            }
+        }
+
+        return separatorList;
+    }
+
+    /**
+     * input => 커스텀 문자 유효성 검증 후 앞 뒤 자르기 ("//", "\n")
+     * @param customArr String
+     * @return Map<String, Boolean> separatorList
+     */
+    private String validCheckSeparator(String customArr) {
+        if (customArr.isEmpty()) {
+            return customArr;
+        } else {
+            // 문자열 앞 뒤 검사 후 숫자 있는지 확인하기
+            if (customArr.startsWith("//") && customArr.endsWith("\\n") && !customArr.matches(".*\\d.*")) {
+                customArr = customArr.substring(2);
+                return customArr.substring(0, customArr.length() - 2);
+            } else {
+                throw new RuntimeException("커스텀 구분자 문자열 형식이 올바르지 않음 : " + customArr);
+            }
+        }
+    }
 
 }
